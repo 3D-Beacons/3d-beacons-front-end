2e8769e3c32b5d24b7c5f4cd554c74c5257686d4
mvaradi <mvaradi@ebi.ac.uk>
2021-02-26 15:13:47 +0000

    Fixing unit test

--- mock-data/mock-summary-categories.ts
+++ mock-data/mock-summary-categories.ts
@@ -1,24 +1,24 @@
 export const MOCK_SUMMARY_CATEGORIES = [
-    {
-        id: 'expr',
-        displayName: 'Experimentally Determined Structures',
-        icon: 'bi-basket',
-        count: 20
-    },
-    {
-        id: 'template',
-        displayName: 'Template-based models',
-        icon: 'bi-basket2',
-        count: 3
-    },
-    {
-        id: 'abinitio',
-        displayName: 'Ab-initio Models',
-        icon: 'bi-basket'
-    },
-    {
-        id: 'ensembl',
-        displayName: 'Conformational Ensembles',
-        icon: 'bi-basket2'
-    }
+  {
+    id: 'expr',
+    displayName: 'Experimentally Determined Structures',
+    icon: 'icon icon-common icon-analyse-percent_100',
+    count: 20
+  },
+  {
+    id: 'ensembl',
+    displayName: 'Conformational Ensembles',
+    icon: 'icon icon-common icon-analyse-percent_40'
+  },
+  {
+    id: 'template',
+    displayName: 'Template-based models',
+    icon: 'icon icon-common icon-systems',
+    count: 3
+  },
+  {
+    id: 'abinitio',
+    displayName: 'Ab-initio Models',
+    icon: 'icon icon-common icon-systems'
+  }
 ];
