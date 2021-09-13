d5507d6ff32e9ee5e725691bd88d8e0e376ef264
Sreenath Sasidharan Nair <33874447+sreenathnair@users.noreply.github.com>
2021-09-13 10:05:57 +0100

    Add ga tracking (#28)
    * PDBE-4636: Added GA tracking
    
    * PDBE-4636: Added dummy gaTag to environment.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+
+declare var gtag;
 
 @Component({
   selector: 'app-root',
@@ -11,7 +15,21 @@ export class AppComponent {
 
   searchTerm = new FormControl('');
 
-  constructor(private router: Router){}
+  constructor(private router: Router) {
+    const navEndEvent$ = router.events.pipe(
+      filter(e => e instanceof NavigationEnd)
+    );
+    navEndEvent$.subscribe((e: NavigationEnd) => {
+      gtag(
+        'config',
+        environment.gaTag,
+        {page_path: e.urlAfterRedirects});
+    });
+    const headerScript = document.createElement('script');
+    headerScript.async = true;
+    headerScript.src = 'https://www.googletagmanager.com/gtag/js?id=' + environment.gaTag;
+    document.head.appendChild(headerScript);
+  }
 
   onSearch() {
     if (this.searchTerm.value.trim() === '') {
