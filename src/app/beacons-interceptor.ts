26c1cd6ed0684602f664323dd3d8d10cc55c8e87
lawal <lawal@ebi.ac.uk>
2026-06-12 03:53:13 +0100

    fixes

--- src/app/beacons-interceptor.ts
+++ src/app/beacons-interceptor.ts
@@ -1,11 +1,19 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 
 @Injectable()
 export class BeaconsInterceptor implements HttpInterceptor {
-    intercept(httpRequest: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        console.debug('Sending request', httpRequest);
-        return next.handle(httpRequest);
-    }
+  intercept(
+    httpRequest: HttpRequest<any>,
+    next: HttpHandler,
+  ): Observable<HttpEvent<any>> {
+    console.debug("Sending request", httpRequest);
+    return next.handle(httpRequest);
+  }
 }
